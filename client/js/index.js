3826c7f5c49fc69ca22a8c5747b62a722c9cb9f2
uires <uiresdeivide1@gmail.com>
2018-11-27 21:46:01 -0200

    Adicionando servidor node e pasta client

---
+++ client/js/index.js
@@ -0,0 +1,8 @@
+var campos = [
+	document.querySelector('#data'),
+	document.querySelector('#quantidade'),
+	document.querySelector('#valor'),
+];
+document.querySelector('.form').addEventListener("submit", function(event) {
+	event.preventDefault();
+});
