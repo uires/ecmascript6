fd197a00c1d73b7a0ca8b9e916b3d338e2fdcd25
uires <uiresdeivide1@gmail.com>
2018-11-27 23:25:44 -0200

    encapsulando atributos

--- client/js/app/model/Negociacao.js
+++ client/js/app/model/Negociacao.js
@@ -1,11 +1,35 @@
 class Negociacao {
 	constructor(data, quantidade, valor) {
-		this.data = data;
-		this.quantidade = quantidade;
-		this.valor = valor;		
+		this._data = data;
+		this._quantidade = quantidade;
+		this._valor = valor;		
 	}
 
-	obtemVolume() {
-		return this.quantidade * this.valor;
+	getData() {
+		return this._data;
+	}
+
+	setData(data) {
+		this._data = data;
+	}
+
+	getQuantidade() {
+		return this._quantidade;
+	}
+
+	setQuantidade(quantidade) {
+		this._quantidade = quantidade;
+	}
+
+	getValor() {
+		return this._valor;
+	}
+
+	setValor(valor) {
+		this._valor = valor;
+	}
+
+	getVolume() {
+		return this._quantidade * this._valor;
 	}
 }
