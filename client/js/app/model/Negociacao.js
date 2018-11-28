89a3f70c12d812baf5688b3e065cf78e1c400af2
uires <uiresdeivide1@gmail.com>
2018-11-28 19:25:13 -0200

    objeto imutável

--- client/js/app/model/Negociacao.js
+++ client/js/app/model/Negociacao.js
@@ -2,31 +2,20 @@ class Negociacao {
 	constructor(data, quantidade, valor) {
 		this._data = data;
 		this._quantidade = quantidade;
-		this._valor = valor;		
+		this._valor = valor;
+		Object.freeze(this);
 	}
 
 	get data() {
 		return this._data;
 	}
 
-	setData(data) {
-		this._data = data;
-	}
-
 	get quantidade() {
 		return this._quantidade;
 	}
 
-	setQuantidade(quantidade) {
-		this._quantidade = quantidade;
-	}
-
 	get valor() {
 		return this._valor;
-	}
-
-	setValor(valor) {
-		this._valor = valor;
 	}
 
 	get volume() {
