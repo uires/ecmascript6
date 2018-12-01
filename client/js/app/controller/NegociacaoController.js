2c09644912b4b11fea8fcde72f015b8f72a905a1
uires <uiresdeivide1@gmail.com>
2018-12-01 20:57:11 -0200

    criando métodos para consumo do controlador

--- client/js/app/controller/NegociacaoController.js
+++ client/js/app/controller/NegociacaoController.js
@@ -4,12 +4,26 @@ class NegociacaoController {
 		this._inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
+		this._listaNegociacoes = new ListaNeogociacao();
 	}
 
 	adicionarNegociacao(event) {
 		event.preventDefault();
+		this._listaNegociacoes.add(this._criarNegociacao());
+		this._limparFormulario();
+	}
+
+
+	_criarNegociacao() {
 		let negociacao = new Negociacao(new Date(DateHelper.tranformStringToDate(this._inputData.value))
 					, this._inputQuantidade.value, this._inputValor.value);
-		console.log(DateHelper.transformDateToString(negociacao.data));
+		return negociacao;
+	}
+
+	_limparFormulario() {
+		this._inputData.value = '';
+		this._inputValor.value = 0.0;
+		this._inputQuantidade.value = 1;
+		this._inputData.focus();
 	}
 }
